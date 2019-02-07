432472e9d662047bcebf16a831ffdb9cf7d3dac7
Timo Staudinger <staudinger.timo@gmail.com>
2019-02-06 23:52:13 -0500

    Update manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,13 +57,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Users matter.`,
+        short_name: `Users matter.`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/profile-pic-round.png`,
       },
     },
     `gatsby-plugin-offline`,
